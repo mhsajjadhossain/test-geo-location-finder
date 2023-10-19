5b12e01c55148aea8c4cc0841b1dda2c09a5df68
M.h Sajjad Hossain Ripon <mail.mhsajjad@gmail.com>
2023-10-19 12:20:52 +0600

    build: done

--- dist/routers/geoIpRouters.js
+++ dist/routers/geoIpRouters.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const geoip = require("geoip-lite");
+const requestIp = require("request-ip");
 const geoIpRouter = (0, express_1.Router)();
-var geoip = require("geoip-lite");
 geoIpRouter.get("/", (req, res) => {
-    const reqJson = req;
-    const clientIP = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    const clientGeoLocation = geoip.lookup(clientIP);
-    res.status(200).json(Object.assign({}, clientGeoLocation));
+    const clientIp = requestIp.getClientIp(req);
+    console.log(clientIp);
+    res.send(clientIp);
 });
 exports.default = geoIpRouter;
